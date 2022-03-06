3b62702a5a9cd18f3da212cbafa40403a8b4667b
Marckie Zeender <mkzeender@gmail.com>
2022-03-06 12:28:10 -0800

    git pull in start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 set RAMM=2G
 set JARFILE=1.18.2.jar
 
+set PATH=%PATH%;%cd%\PortableGit\cmd
+
 git pull
 
 java -Xms%RAMM% -Xmx%RAMM% -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar %JARFILE% nogui
