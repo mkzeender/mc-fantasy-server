0648569ef6a53d12973f1f75d4ffa3f9ce84ef8f
Marckie Zeender <mkzeender@gmail.com>
2022-03-06 12:09:03 -0800

    added server files

---
+++ start.bat
@@ -0,0 +1,7 @@
+set RAMM=2G
+set JARFILE=server.jar
+
+git pull
+
+java -Xms%RAMM% -Xmx%RAMM% -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar %JARFILE% nogui
+
