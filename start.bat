30680ffad4203afda68d176726ddc96e36f7fcee
Marckie Zeender <mkzeender@gmail.com>
2022-03-07 22:04:52 -0800

    another change

--- start.bat
+++ start.bat
@@ -5,9 +5,9 @@ set JARFILE=1.18.2.jar
 
 set PATH=%PATH%;%cd%\PortableGit\cmd
 
-copy /Y settings.txt server.properties
-
 git pull
+
+copy /Y settings.txt server.properties
 
 java -Xms%RAMM% -Xmx%RAMM% -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar %JARFILE% nogui
 
