1bb616da2d12e230bf457deaab5bb9b5d89e5632
Marckie Zeender <mkzeender@gmail.com>
2022-03-07 22:04:13 -0800

    changes to start.bat

--- start.bat
+++ start.bat
@@ -11,3 +11,4 @@ git pull
 
 java -Xms%RAMM% -Xmx%RAMM% -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar %JARFILE% nogui
 
+pause > nul
